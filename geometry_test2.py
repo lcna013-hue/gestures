9426ebb4de7ef0af2b47dd66d655ce6cc8df84e0
Lucy Black <lcna013@gmail.com>
2026-06-13 14:54:48 +1000

    Pinch image shows on GUI

--- geometry_test2.py
+++ geometry_test2.py
@@ -1,5 +1,6 @@
-from tkinter import Label
 import tkinter
+from tkinter import Image
+from tkinter import Label
 from tkinter import PhotoImage
 root = tkinter.Tk()
 root.title('Gesture Photo Capturer ')
@@ -16,6 +17,10 @@ frame_change = tkinter.Frame(root)
 def passx():
     pass
 
+pinch_img = PhotoImage(file="pinch_gesutre_img.png")
+pinch_lbl = tkinter.Label(root, image=pinch_img).grid()
+
+
 frame_title_home.grid(row=0, column=0, padx=(750, 0))
 title = tkinter.Label(frame_title_home, text="HOME", font=("Helvetica", 30))
 title.grid(pady=(20, 10))
@@ -25,5 +30,7 @@ gesutre with your hand.""", font=("Helvetica", 20))
 start_date_lbl.grid(column=0, row=0, pady=15)
 end_date_lbl = tkinter.Label(frame_fields, text="text 2", font=("Helvetica", 20))
 end_date_lbl.grid(column=0, row=1)
+
+label_img = Image
 
 root.mainloop()
